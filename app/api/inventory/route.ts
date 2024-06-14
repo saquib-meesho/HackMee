365abc663f3b4ce0b5cf842eda702f8a7d4524dc
Harshdeep-Meesho <harshdeep.shrivastava@meesho.com>
2024-06-14 09:34:13 +0530

    api url

--- app/api/inventory/route.ts
+++ app/api/inventory/route.ts
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server";
 
 const GET = async (req: Request) => {
     try {
-        const res = await fetch('localhost:5000/fetchAll');
+        const res = await fetch('http://localhost:5000//getAll');
         const data = await res.json();
-        return NextResponse.json({data: data}, {status: 200})
+        
+        return NextResponse.json({data: data.result}, {status: 200})
     } catch (error) {
         console.log(error);
         return NextResponse.json({message: "Server error"}, {status: 500});
--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -23,7 +23,7 @@ const HomePage = () => {
     <div className="mx-auto py-4 px-40">
       <h1 className="text-2xl font-bold mb-4 text-gray-700">Men's Clothing</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {product.map(product => (
+        {product?.map(product => (
           <ProductTile key={product.product_id} item={product} />
         ))}
       </div>
